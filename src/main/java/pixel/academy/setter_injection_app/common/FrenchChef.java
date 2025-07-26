a018425897a5ddd14788a645518d15caef819702
Cojocaru Adrian <adrianmail1@gmail.com>
2025-07-26 12:05:43 +0300

    Second commit

--- src/main/java/pixel/academy/setter_injection_app/common/FrenchChef.java
+++ src/main/java/pixel/academy/setter_injection_app/common/FrenchChef.java
@@ -3,12 +3,13 @@ package pixel.academy.setter_injection_app.common;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Primary
 @Component
 public class FrenchChef implements Chef {
 
     public FrenchChef(){
         System.out.println("In constructor"+ getClass().getSimpleName());
+        System.out.println("Empty"+ getClass().getSimpleName());
     }
     @Override
     public String getDailyRecipe() {
--- src/main/java/pixel/academy/setter_injection_app/common/ItalianChef.java
+++ src/main/java/pixel/academy/setter_injection_app/common/ItalianChef.java
@@ -1,6 +1,7 @@
 package pixel.academy.setter_injection_app.common;
 
 
+import ch.qos.logback.classic.spi.PlatformInfo;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,7 @@ public class ItalianChef implements Chef {
 
     public ItalianChef(){
         System.out.println("In constructor"+ getClass().getSimpleName());
-
+        System.out.println("Plin");
         }
     @Override
     public String getDailyRecipe() {
